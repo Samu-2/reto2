4e08d0826e94239a2119f13d7f052f199e7c250f
hugo926 <hugoclaveria99@gmail.com>
2024-10-31 09:42:00 +0100

    Funciones hechas Merge branch 'main' of https://github.com/Samu-2/reto2.git into main

--- src/retos/Funciones2.java
+++ src/retos/Funciones2.java
@@ -1,25 +1,84 @@
 package retos;
 
+import java.util.Scanner;
+
 public class Funciones2 {
 	
-	public static double gradosAFahrenheit(double grados) {
+	public static void menuTemperatura() //submenu
+	{
+		Scanner sc= new Scanner(System.in); //PA LEER TODO
+		int opcion=0; //opcion que elige 
+		do {
+			System.out.println("Selecciona la conversion de longitud:");
+			System.out.println("1.De grados a fahrenheit");
+			System.out.println("2.De fahrenheit a grados");
+			System.out.println("0.Salir");
+			
+		switch(opcion) {
+		case 1: 
+		double grados=FuncionesValidar.dimeDouble("Introduce grados", sc);
+			System.out.println(Funciones2.gradosAFahrenheit(grados));
+			
+		case 2: 
+			double fahrenheit=FuncionesValidar.dimeDouble("Introduce fahrenheit", sc);
+				System.out.println(Funciones2.fahrenheitAGrados(fahrenheit));
+				
+		case 0:
+	
+			default:
+		}
+			
+		
+		}while(opcion!=0);
+		
+	}
+	public static double gradosAFahrenheit(double grados) { //funcion
 		
 		return ((grados * 9 / 5) + 32);
 	}
 	
-    public static double fahrenheitAGrados(double fahrenheit) {
+    public static double fahrenheitAGrados(double fahrenheit) { //funcion
 		
     	return ((fahrenheit - 32) * 5 / 9);
 	}
     
-    public static double litrosAGalones(double litros) {
+    public static void menuVolumen() //submenu
+	{
+    	Scanner sc= new Scanner(System.in);
+    	int opcion=0;
+		do {
+			System.out.println("Selecciona la conversion de longitud:");
+			System.out.println("1.De litros a galones");
+			System.out.println("2.De galones a litros");
+			System.out.println("0.Salir");
+			
+			switch(opcion) {
+			case 1: 
+			double litros=FuncionesValidar.dimeDouble("Introduce litros", sc);
+				System.out.println(Funciones2.litrosAGalones(litros));
+				
+			case 2: 
+				double galones=FuncionesValidar.dimeDouble("Introduce galones", sc);
+					System.out.println(Funciones2.galonesALitros(galones));
+					
+			case 0:
+		
+				default:
+			}
+
+		}while(opcion!=0); //cuando sea 0 se acaba
+		
+	}
+    public static double litrosAGalones(double litros) { //funcion
     	
     	return litros/3.785;
     }
     
-    public static double galonesALitros(double galones) {
+    public static double galonesALitros(double galones) { //funcion
     	
     	return galones*3.785;
     }
+    
+    
 
 }
