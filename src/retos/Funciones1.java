85acccb1dfd28f81be204e52bb730dd4e1b9b71d
Samu-2 <samuel.m27a@gmail.com>
2024-10-31 09:17:52 +0100

    funciones benas

--- src/retos/Funciones1.java
+++ src/retos/Funciones1.java
@@ -1,13 +1,40 @@
 package retos;
 
+import java.util.Scanner;
+
 public class Funciones1 {
-public static void longitud() {
+public static void menuLongitud() {
+	Scanner sc = new Scanner(System.in);
+
+	int opcion=0;
 	do {
 		System.out.println("Selecciona la conversion de longitud:");
 		System.out.println("1.Millas a Kilómetros");
 		System.out.println("2.Kiloóetros a millas");
 		System.out.println("0.Salir");
-	}while(true);
+	    opcion =FuncionesValidar.dimeEntero("Elige una opción", sc);
+		
+		switch(opcion) {
+		case 1:
+			System.out.println("Introduce Millas");
+			String millas=sc.nextLine();
+			int millas1=Integer.parseInt(millas);
+			Funciones1.dimeKilometros(millas1);
+		case 2:
+			System.out.println("Introduce kilometros");
+			String kilometros=sc.nextLine();
+			int kilometros1=Integer.parseInt(kilometros);
+			System.out.println(Funciones1.dimeMillas(kilometros1));
+		case 0:
+			default:
+		}
+		
+			
+			
+			
+			
+	}while(opcion!=0);
+
 }
 public static double dimeMillas(double kilometros) {
 	 return kilometros/1.609;
@@ -15,7 +42,7 @@ public static double dimeMillas(double kilometros) {
 public static double dimeKilometros(double millas) {
 	return millas*1.609;
 }
-public static void peso() {
+public static void menuPeso() {
 	do {
 		System.out.println("Selecciona la conversion de peso:");
 		System.out.println("1.Libras a lilogramos");
--- src/retos/Reto2.java
+++ src/retos/Reto2.java
@@ -1,9 +1,29 @@
 package retos;
 
+import java.util.Scanner;
+
 public class Reto2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int opcion=0;
+		do {
+		System.out.println("Selecciona el tipo de conversion");
+		System.out.println("1.Longitud");
+		System.out.println("2.Peso");
+		System.out.println("3.Temperatura");
+		System.out.println("4.Volumen");
+		System.out.println("0.Salir");
+		opcion =FuncionesValidar.dimeEntero("Elige una opción", sc);
+		
+		switch(opcion) {
+		case 1:
+			Funciones1.menuLongitud();
+		}
+	
+		
+		}while(opcion!=0);
 
 	}
 
