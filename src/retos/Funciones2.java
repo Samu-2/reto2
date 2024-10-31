60cddf3e33a2c0f2a17f00dd9e6f545bcc51b91b
hugo926 <hugoclaveria99@gmail.com>
2024-10-31 08:44:05 +0100

    todas las funciones

--- src/retos/Funciones2.java
+++ src/retos/Funciones2.java
@@ -1,5 +1,25 @@
 package retos;
 
 public class Funciones2 {
+	
+	public static double gradosAFahrenheit(double grados) {
+		
+		return ((grados * 9 / 5) + 32);
+	}
+	
+    public static double fahrenheitAGrados(double fahrenheit) {
+		
+    	return ((fahrenheit - 32) * 5 / 9);
+	}
+    
+    public static double litrosAGalones(double litros) {
+    	
+    	return litros/3.785;
+    }
+    
+    public static double galonesALitros(double galones) {
+    	
+    	return galones*3.785;
+    }
 
 }
