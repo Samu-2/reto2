bd138fce65bbc5bd28299e221d4ea8cca421dc6e
Samu-2 <samuel.m27a@gmail.com>
2024-10-31 09:20:45 +0100

    funciones hechas

--- src/retos/Funciones1.java
+++ src/retos/Funciones1.java
@@ -19,7 +19,8 @@ public static void menuLongitud() {
 			System.out.println("Introduce Millas");
 			String millas=sc.nextLine();
 			int millas1=Integer.parseInt(millas);
-			Funciones1.dimeKilometros(millas1);
+		System.out.println(Funciones1.dimeKilometros(millas1));	
+			
 		case 2:
 			System.out.println("Introduce kilometros");
 			String kilometros=sc.nextLine();
@@ -43,12 +44,17 @@ public static double dimeKilometros(double millas) {
 	return millas*1.609;
 }
 public static void menuPeso() {
+	Scanner sc = new Scanner(System.in);
+
+	int opcion=0;
 	do {
 		System.out.println("Selecciona la conversion de peso:");
 		System.out.println("1.Libras a lilogramos");
 		System.out.println("2.Kilogramos a libras");
 		System.out.println("0.Salir");
-	}while(true);
+		opcion =FuncionesValidar.dimeEntero("Elige una opción", sc);
+		
+	}while(opcion!=0);
 	
 }
 public static double dimeKilogramos(double libras) {
